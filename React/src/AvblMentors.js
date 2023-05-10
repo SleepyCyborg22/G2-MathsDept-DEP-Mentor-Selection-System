1cbce25304e47c732a1e0bcde28448a447e44185
Ashvin324 <94772961+Ashvin324@users.noreply.github.com>
2023-05-10 15:48:58 +0530

    Update AvblMentors.js

--- React/src/AvblMentors.js
+++ React/src/AvblMentors.js
@@ -1,3 +1,16 @@
+import MiniProfile from "./MiniProfile"
+import Profileimg from './Alanson_Headshot.jpg'
+
 export default function AvblMentors() {
-    
+    return (
+        <div>
+            <MiniProfile
+                profileImg={Profileimg}
+                name='XYZ'
+                interests='Coding,Cooking'
+                email='XYZ@gmail.com'
+                phone='1234567890'
+            />
+        </div>
+    )
 }
