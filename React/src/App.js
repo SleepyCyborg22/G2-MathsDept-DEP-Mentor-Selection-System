e408b45e2b00d2127fef8adccec9c308cd225bd2
Ashvin324 <94772961+Ashvin324@users.noreply.github.com>
2023-05-10 15:48:41 +0530

    Update App.js

--- React/src/App.js
+++ React/src/App.js
@@ -1,11 +1,10 @@
+import NavBar from './Navbar';
 import './styles.css';
-import Navbar from './Navbar';
 
 export default function App() {
   return (
     <div>
-      <h1>A</h1>
-    <Navbar/>
+      <NavBar/>
     </div>
   );
 }
